f697d2fcb769bf4818ce0b1bd5da5bddedf59ac6
collett <collett0301@gmail.com>
2020-11-11 02:57:40 +0900

    fix merge mistake

--- src/world/Actor/Player.h
+++ src/world/Actor/Player.h
@@ -1024,6 +1024,28 @@ namespace Sapphire::Entity
     Common::SamSen gaugeSamGetSenRaw();
     bool gaugeSamHasAnySen();
 
+    // party
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+  private:
+    PlayerPtr m_partyLeader;
+    PlayerPtr m_partyInvitationSender;
+    std::vector< PlayerPtr > m_partyMemberList;
+    void clearPartyList();
+    void sendPartyListToParty();
+  public:
+    bool isPartyLeader();
+    bool isInParty();
+    bool createEmptyParty();
+    void disbandParty();
+    PlayerPtr getPartyLeader();
+    PlayerPtr getPartyInvitationSender();
+    void setPartyInvitationSender( PlayerPtr sender );
+    bool addPartyMember( PlayerPtr member );
+    bool removePartyMember( PlayerPtr member );
+    bool changePartyLeader( PlayerPtr newLeader );
+    uint8_t getPartySize();
+    void foreachPartyMember( std::function< void( PlayerPtr member ) > callback);
+
     //////////////////////////////////////////////////////////////////////////////////////////////////////
 
     void setPosAndSendActorMove( float x, float y, float z, float rot );
