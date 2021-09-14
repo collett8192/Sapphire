0ff2f4cdb14673abacae1a76b98a4dc162689a03
collett <collett0301@gmail.com>
2021-09-14 23:37:40 +0900

    fix get pos

--- src/world/Manager/DebugCommandMgr.cpp
+++ src/world/Manager/DebugCommandMgr.cpp
@@ -684,7 +684,7 @@ void Sapphire::World::Manager::DebugCommandMgr::get( char* data, Entity::Player&
         instance->getBranch() );
     }
   }
-  if( ( subCommand == "poprange" ) )
+  else if( ( subCommand == "poprange" ) )
   {
     uint32_t id, param;
     sscanf( params.c_str(), "%u", &id );
